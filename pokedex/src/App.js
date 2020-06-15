72481ba207ac4e1a440ae78b59ad71c915a0983d
amadou saidy <ahmo300@outlook.com>
2020-06-16 01:09:48 +0200

    I added a new component pokedexarrow

--- pokedex/src/App.js
+++ pokedex/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Header from './components/Header';
 import Pokedex from './components/Pokedex';
+import PokedexArrow from './components/PokedexArrow';
 
 function App() {
   return <div>
     <Header/>
     <Pokedex/>
+    <PokedexArrow/>
   </div>;
 }
 
---
+++ pokedex/src/components/PokedexArrow.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const PokedexArrow = () => {
+  return <div>
+    <h2>this is my PokedexArrow</h2>
+    
+<ul>
+    <li>mikachu</li>
+    <li>squirtel</li>
+</ul>
+
+  </div>;
+}
+
+export default PokedexArrow;
